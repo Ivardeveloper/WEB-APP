96411cb7528f533aae337fba94076b80eb4f9be3
IvanMza <84762526+Ivardeveloper@users.noreply.github.com>
2023-07-02 15:32:05 -0600

    Add files via upload
    Primer Commit

---
+++ AngularSwaggerRest/Program.cs
@@ -0,0 +1,39 @@
+
+using System;
+using System.Net;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.RedirectStatusCode = (int)HttpStatusCode.TemporaryRedirect;
+    options.HttpsPort = 5001;
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseHttpsRedirection();
+}
+
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseHttpsRedirection();
+app.Run();
